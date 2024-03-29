9d3d1cb2e3bcfe2908d20d23f8da5c8f209b709d
Allan-njoroge <allandavis@gmail.com>
2024-03-29 23:14:39 +0300

    Added data to cards

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,16 +8,16 @@ import { FaDownload } from "react-icons/fa";
 
 const dashboard = () => {
   return (
-    <div className="flex">
+    <div className="flex w-full">
       <Sidebar />
       <div className="ml-[5%] w-full">
-        <div className="mx-10">
-          <div className="flex justify-between mt-10">
-            <h1 className="font-bold text-2xl">DASHBOARD OVERVIEW</h1>
+        <div className="mx-10 w-full">
+          <div className="flex flex-wrap justify-between mt-10 gap-3">
+            <h1 className="font-bold md:text-2xl">DASHBOARD OVERVIEW</h1>
             <Button className="my-auto"><FaDownload className="mr-3" /> Download Report</Button>
           </div>
 
-          <div className="mt-5 lg:mt-8">
+          <div className="mt-5 lg:mt-8 mx-auto w-full">
             <Cards/>
           </div>
         </div>
